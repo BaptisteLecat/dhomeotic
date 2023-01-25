94c3ed7c52c133750d7a94401b2d51bf55ff17d8
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-01-25 16:32:32 +0100

    test notif

--- dhomeotic/lib/main.dart
+++ dhomeotic/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dhomeotic/firebase_options.dart';
 import 'package:dhomeotic/presentation/app/app.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -8,6 +9,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  //Check if the user as authorized the app to send notifications.
+
+  //create a doc test in the collection test
+  await FirebaseFirestore.instance
+      .collection('test')
+      .doc('test')
+      .set({'test': 'test'});
+  print('Handling a background message ${message.messageId}');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -15,6 +29,7 @@ void main() async {
   );
   final fcmToken = await FirebaseMessaging.instance.getToken();
   log('FCM token: $fcmToken');
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   BlocOverrides.runZoned(
     () => initializeDateFormatting('fr_FR', null)
         .then((_) => runApp(const AppRoot())),
